65ff749d56bdd3670987abba077c55003a54fa83
ARtheboss <30683624+ARtheboss@users.noreply.github.com>
2019-11-24 13:29:25 +0800

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,3 +1,4 @@
+
 String.prototype.hashCode = function() {
     var hash = 0;
     if (this.length == 0) {
@@ -26,18 +27,20 @@ function getJSONfile(url){
 
 function signup(){
 	if(document.getElementById('password').value == document.getElementById('cpassword').value){
-		getJSONfile('http://3.14.29.188:5000/get_info?signup='+document.getElementById('username').value+'&hash='+document.getElementById('password').value.hashCode())
+		getJSONfile('http://0.0.0.0:5000/get_info?signup='+document.getElementById('username').value+'&hash='+document.getElementById('password').value.hashCode())
 	}else{
 		alert("Password don't match!")
 	}
 }
 
 function login(){
-	getJSONfile('http://3.14.29.188:5000/get_info?login='+document.getElementById('username').value+'&hash='+document.getElementById('password').value.hashCode())
+	getJSONfile('http://0.0.0.0:5000/get_info?login='+document.getElementById('username').value+'&hash='+document.getElementById('password').value.hashCode())
 }
 
 function result(x){
 	if(x == "1"){
+		sessionStorage.setItem("logged_in", "1");
+		sessionStorage.setItem('username',document.getElementById('username').value,5);
 		window.location.href = "../index.html";
 	}else{
 		alert("Incorrect Password!")
